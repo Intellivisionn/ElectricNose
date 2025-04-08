95735fd8f384fa3fa366d2f55652ff0da7b6516b
martpec <95352759+martpec@users.noreply.github.com>
2025-04-08 14:49:33 +0200

    change SGP again

--- Sensors/SGP30Sensor.py
+++ Sensors/SGP30Sensor.py
@@ -4,11 +4,13 @@ import adafruit_sgp30
 class SGP30Sensor(Sensor):
     def __init__(self, i2c):
         self.sensor = adafruit_sgp30.Adafruit_SGP30(i2c)
+        self.sensor.iaq_init()
         self.sensor.set_iaq_baseline(0x8973, 0x8AAE)
         self.sensor.set_iaq_relative_humidity(celsius=22.1, relative_humidity=44)
 
     def read_data(self):
+        eco2, tvoc = self.sensor.iaq_measure()
         return {
-            "CO2": self.sensor.eCO2,
-            "TVOC": self.sensor.TVOC
+            "CO2": eco2,
+            "TVOC": tvoc
         }
