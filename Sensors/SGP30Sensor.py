7ae99927c59860784c9461314a05514958f246b1
martpec <95352759+martpec@users.noreply.github.com>
2025-04-10 18:56:27 +0200

    change SGP once again

--- Sensors/SGP30Sensor.py
+++ Sensors/SGP30Sensor.py
@@ -5,18 +5,16 @@ class SGP30Sensor(Sensor):
     def __init__(self, i2c):
         self.sensor = adafruit_sgp30.Adafruit_SGP30(i2c)
         self.sensor.iaq_init()
-        self.sensor.set_iaq_baseline(0x8973, 0x8AAE)
-        self.sensor.set_iaq_relative_humidity(celsius=22.1, relative_humidity=44)
+        self.sensor.set_iaq_relative_humidity(celsius=28, relative_humidity=28)
 
     def read_data(self):
         eco2, tvoc = self.sensor.iaq_measure()
-	h2, ethanol = self.sensor.raw_measure()
-	eco2_base, tvoc_base = self.sensor.get_iaq_baseline()
+        h2, ethanol = self.sensor.raw_measure()
+        baseline = self.sensor.get_iaq_baseline()
         return {
-            "CO2": eco2,
-            "TVOC": tvoc,
-	    "BASELINE CO2": eco2_base,
-	    "BASELINE TVOC": tvoc_base,
-	    "Raw H2": h2,
-	    "Raw Ethanol": ethanol
-        }
+                "CO2": eco2,
+                "TVOC": tvoc,
+                "BASELINE": baseline,
+                "Raw H2": h2,
+                "Raw Ethanol": ethanol
+                }
