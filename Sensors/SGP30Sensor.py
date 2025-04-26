b58bb80a5e1024c8c99cf873ea268c42c85facae
JakubPotocky <77487846+JakubPotocky@users.noreply.github.com>
2025-04-26 18:12:39 +0200

    Fix SGP30Sensor.py

--- Sensors/SGP30Sensor.py
+++ Sensors/SGP30Sensor.py
@@ -5,7 +5,7 @@ class SGP30Sensor(Sensor):
     def __init__(self, i2c):
         self.sensor = adafruit_sgp30.Adafruit_SGP30(i2c)
         self.sensor.iaq_init()
-        self.sensor.set_iaq_baseline(0x84D0, 0x86C4)
+        self.sensor.set_iaq_baseline(0x8E8F, 0x944A)
         self.sensor.set_iaq_relative_humidity(celsius=28, relative_humidity=28)
 
     def read_data(self):
