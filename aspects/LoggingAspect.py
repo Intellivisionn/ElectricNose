e0d0924af4448c10fc2bcbec2022c2f0aac8fca2
svenons <gotvonisvens@gmail.com>
2025-04-26 13:29:41 +0200

    Error Fixes

--- aspects/LoggingAspect.py
+++ aspects/LoggingAspect.py
@@ -3,11 +3,11 @@ class LoggingAspect:
     def log_method(func):
         def wrapper(*args, **kwargs):
             # [LOG] Before calling {className}.{methodName}()
-            print(f"[LOG] Before calling {(args[0] if args else None).__class__.__name__ if (args[0] if args else None) else "UnknownClass"}.{func.__name__}()")
+            print(f"[LOG] Before calling {(args[0].__class__.__name__ if (args and args[0]) else 'UnknownClass')}.{func.__name__}()")
             try:
                 result = func(*args, **kwargs)
             finally:
                 # [LOG] After calling {className}.{methodName}()
-                print(f"[LOG] After calling {(args[0] if args else None).__class__.__name__ if (args[0] if args else None) else "UnknownClass"}.{func.__name__}()")
+                print(f"[LOG] After calling {(args[0].__class__.__name__ if (args and args[0]) else 'UnknownClass')}.{func.__name__}()")
             return result
         return wrapper
