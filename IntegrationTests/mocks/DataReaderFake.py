4656ab9698368203be7c90bcd0b7a5037366de75
svenons <gotvonisvens@gmail.com>
2025-05-02 15:10:31 +0200

    test fix 10

--- IntegrationTests/mocks/DataReaderFake.py
+++ IntegrationTests/mocks/DataReaderFake.py
@@ -1,11 +1,13 @@
 from DataReader.source.data_reader import SensorDataCollector
+from pathlib import Path
 
 if __name__ == "__main__":
     # Specify the path to the JSON file containing sensor data
-    sensor_json_file = "/home/admin/ElectricNose/IntegrationTests/mocks/sensor_data.json"
+    sensor_json_file = Path(__file__).resolve().parent / "sensor_data.json"
+    output_dir = Path(__file__).resolve().parent / "savedData"
     
     # Create an instance of the SensorDataCollector
-    collector = SensorDataCollector(sensor_json_file, read_interval=2, write_interval=5, output_dir="/home/admin/ElectricNose/IntegrationTests/savedData")
+    collector = SensorDataCollector(sensor_json_file, read_interval=2, write_interval=5, output_dir=output_dir)
     
     # Start collecting and writing data
     collector.start()
