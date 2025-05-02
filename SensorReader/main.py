936929770ef9147b7b6444861cffcb48509656ad
svenons <gotvonisvens@gmail.com>
2025-05-02 13:13:34 +0200

    remake the SensorReader to a more modular approach for testing

--- SensorReader/main.py
+++ SensorReader/main.py
@@ -1,26 +1,18 @@
 import time
 import json
+from pathlib import Path
+from SensorReader.Sensors.SensorManager import SensorManager
+from SensorReader.aspects.LoggingAspect import LoggingAspect
 import board
 from smbus2 import SMBus
-from pathlib import Path
-
-# Import sensor classes
 from SensorReader.Sensors.BME680Sensor import BME680Sensor
 from SensorReader.Sensors.SGP30Sensor import SGP30Sensor
 from SensorReader.Sensors.GroveGasSensor import GroveGasSensor
-from SensorReader.Sensors.SensorManager import SensorManager
-from SensorReader.aspects.LoggingAspect import LoggingAspect
 
 
-class ElectricNoseSensorReader:
-    def __init__(self, output_path, sleep_interval=2):
-        self.i2c = board.I2C()
-        self.bus = SMBus(1)
-        self.sensors = [
-            BME680Sensor(self.i2c),
-            SGP30Sensor(self.i2c),
-            GroveGasSensor(self.bus)
-        ]
+class BaseSensorReader:
+    def __init__(self, sensors, output_path, sleep_interval=2):
+        self.sensors = sensors
         self.manager = SensorManager(self.sensors)
         self.output_path = output_path
         self.sleep_interval = sleep_interval
@@ -29,17 +21,25 @@ class ElectricNoseSensorReader:
     def read_and_save(self):
         while True:
             self.read_and_save_once()
-
             time.sleep(self.sleep_interval)
 
     def read_and_save_once(self):
         data = self.manager.read_all()
         sensor_json = json.dumps(data, indent=4)
-        #            print(sensor_json)
-
         with open(self.output_path, "w") as f:
             f.write(sensor_json)
 
+
+class ElectricNoseSensorReader(BaseSensorReader):
+    def __init__(self, output_path, sleep_interval=2):
+        i2c = board.I2C()
+        bus = SMBus(1)
+        sensors = [
+            BME680Sensor(i2c),
+            SGP30Sensor(i2c),
+            GroveGasSensor(bus)
+        ]
+        super().__init__(sensors, output_path, sleep_interval)
 
 if __name__ == "__main__":
     output_path = Path.cwd() / "sensor_data.json"
